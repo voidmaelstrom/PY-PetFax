8a745eccc11c7d74c574304fbe681b35998a6c26
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-06-13 16:39:26 -0600

    Up through Activity (ppp-5-blueprints-and-views)

---
+++ petfax/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+def create_app(): 
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    # return the app
+    return app
